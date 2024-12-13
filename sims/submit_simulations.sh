0e406d1fd2d314563ecdb2c5947807c208fb66d0
Joel Eliason <joel.eliason119@gmail.com>
2024-12-13 13:02:34 -0700

    500 sims.

--- sims/submit_simulations.sh
+++ sims/submit_simulations.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --job-name mm_sim
-#SBATCH --array=0-1
+#SBATCH --array=1-499
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1:00:00
-#SBATCH --mem=4G
+#SBATCH --mem=2G
 #SBATCH --mail-user=joelne@umich.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=slurm_logs/slurm_%a.out
 
 source ~/virtual_envs/physicell/bin/activate
-python -u sims/run_simulation.py --theta_id $SLURM_ARRAY_TASK_ID --n_samples 2 --seed_start 1234 > slurm_logs/script_$SLURM_ARRAY_TASK_ID.log
+python -u sims/run_simulation.py --theta_id $SLURM_ARRAY_TASK_ID --n_samples 500 --seed_start 1234 > slurm_logs/script_$SLURM_ARRAY_TASK_ID.log
