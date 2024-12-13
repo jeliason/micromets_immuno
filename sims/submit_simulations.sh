4fb145fc5e6d4569e6dde7bc68113570ef84dc42
Joel Eliason <joel.eliason119@gmail.com>
2024-12-13 10:58:21 -0700

    need more mem.

--- sims/submit_simulations.sh
+++ sims/submit_simulations.sh
@@ -4,7 +4,7 @@
 #SBATCH --array=0-1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1:00:00
-#SBATCH --mem=1G
+#SBATCH --mem=4G
 #SBATCH --mail-user=joelne@umich.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=slurm_logs/slurm_%a.out
